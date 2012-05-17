a175ad1fa9e56302cf4874a5459b5b2c3fc9cbcf
igvel <igvel@mail.ru>
2012-05-17 14:34:00 +0400

    End of Chapter 3. Created domain model.

---
+++ hubbub/grails-app/domain/com/ivel/Post.groovy
@@ -0,0 +1,18 @@
+package com.ivel
+
+class Post {
+	String content
+	Date dateCreated
+	
+	static constraints = {
+		content(blank: false)
+	}
+	
+	static belongsTo = [ user : User ]
+	
+	static hasMany = [ tags : Tag ]
+	
+	static mapping = {
+		sort dateCreated:"desc"
+	}
+}
---
+++ hubbub/grails-app/domain/com/ivel/Tag.groovy
@@ -0,0 +1,13 @@
+package com.ivel
+
+class Tag {
+
+	String name
+	User user
+	
+	static constraints = { name(blank: false) }
+	
+	static hasMany = [ posts : Post ]
+	
+	static belongsTo = [User, Post]
+}

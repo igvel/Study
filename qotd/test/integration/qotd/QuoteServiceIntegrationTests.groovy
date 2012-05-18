affc83a779e96a82efa5a88b5366fcde3f8b40df
igvel <igvel@mail.ru>
2012-05-18 17:19:25 +0400

    Added Quote of The Day study application

---
+++ qotd/test/integration/qotd/QuoteServiceIntegrationTests.groovy
@@ -0,0 +1,12 @@
+package qotd
+
+import grails.test.*
+
+class QuoteServiceIntegrationTests extends GroovyTestCase {
+	def quoteService
+	void testStaticQuote() {
+		def staticQuote = quoteService.getStaticQuote()
+		assertEquals("Anonymous", staticQuote.author)
+		assertEquals("Real Programmers Don't eat Quiche", staticQuote.content)
+	}
+}

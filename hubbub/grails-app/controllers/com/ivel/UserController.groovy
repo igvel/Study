5482f668ac6b941a69b0bec14597311559347e28
igvel <igvel@mail.ru>
2012-05-21 16:40:13 +0400

    Added User registration.

--- hubbub/grails-app/controllers/com/ivel/UserController.groovy
+++ hubbub/grails-app/controllers/com/ivel/UserController.groovy
@@ -35,4 +35,69 @@ class UserController {
 		}
 		[ profiles : profiles ]
 	}
+
+	// direct binding of params
+	def register = {
+		def user = new User(params)
+		if (user.validate()) {
+			user.save()
+			flash.message = "Successfully Created User"
+			redirect(uri: '/')
+		} else {
+			flash.message = "Error Registering User"
+			return [ user: user ]
+		}
+	}
+
+	// binding using command
+	def register2 = { UserRegistrationCommand urc ->
+		if (urc.hasErrors()) {
+			[user : urc]
+		} else {
+			def user = new User(urc.properties)
+			user.profile = new Profile(urc.properties)
+			if (user.save()) {
+				flash.message = "Welcome aboard, ${urc.fullName ?: urc.userId}"
+				redirect(uri: '/')
+			} else {
+				// maybe not unique userId?
+				[user : urc]
+			}
+		}
+	}
 }
+
+class UserRegistrationCommand {
+	String userId
+	String password
+	String passwordRepeat
+	byte[] photo
+	String fullName
+	String bio
+	String homepage
+	String email
+	String timezone
+	String country
+	String jabberAddress
+	
+	static constraints = {
+		userId(size: 3..20)
+		password(size: 6..8, blank: false,
+				validator: { passwd, urc ->
+					return passwd != urc.userId
+				})
+		passwordRepeat(nullable: false,
+				validator: { passwd2, urc ->
+					return passwd2 == urc.password
+				})
+		fullName(nullable: true)
+		bio(nullable: true, maxSize: 1000)
+		homepage(url: true, nullable: true)
+		email(email: true, nullable: true)
+		photo(nullable: true)
+		country(nullable: true)
+		timezone(nullable: true)
+		jabberAddress(email: true, nullable: true)
+	}
+}
+

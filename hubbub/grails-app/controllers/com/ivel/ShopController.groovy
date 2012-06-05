3bd5390d6423894ca74353ff70bcb794e5bd3f04
igvel <igvel@mail.ru>
2012-06-05 15:05:30 +0400

    Aded Shop controller used to study Web Flow.

---
+++ hubbub/grails-app/controllers/com/ivel/ShopController.groovy
@@ -0,0 +1,48 @@
+package com.ivel
+
+class ShopController {
+
+	def index = {  redirect(action: "order") }
+
+	def orderFlow = {
+		displayProducts {
+			on("next") {
+				// capture products
+			}.to("enterAddress")
+			on("cancel").to("finish")
+		}
+		enterAddress {
+			on("next") {
+				// capture address
+			}.to("enterPayment")
+			on("previous").to("displayProducts")
+		}
+		enterPayment {
+			on("next") {
+				// capture payment details
+			}.to("validateCard")
+			on("previous").to("enterAddress")
+		}
+		validateCard {
+			action {
+				// do some logic here
+				if (params.validCard) {
+					log.debug "Valid Card!!"
+					valid()
+				} else {
+					log.debug "Invalid Card!!"
+					invalid()
+				}
+			}
+			on("valid").to("orderComplete")
+			on("invalid").to("enterPayment")
+		}
+		orderComplete {
+			// display order
+			on("finished").to("finish")
+		}
+		finish {
+			redirect(controller:"homePage", action: "index")
+		}
+	}
+}
